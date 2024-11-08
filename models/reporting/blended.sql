f5c1f6bec2ab99862053b939c6c60add043aa1ff
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-11-08 14:19:07 -0600

    Update blended.sql
    {% if not loop.last %}UNION ALL
            {% endif %}
        {% endfor %}

--- models/reporting/blended.sql
+++ models/reporting/blended.sql
@@ -13,26 +13,32 @@ WITH initial_bookings_data as
     FROM {{ source('gsheet_raw','bookings_completed') }} ),
 
 bookings_data as
-    ({%- for date_granularity in date_granularity_list %}    
+  ({%- for date_granularity in date_granularity_list %}    
     SELECT '{{date_granularity}}' as date_granularity, {{date_granularity}} as date,
         CASE WHEN url ~* 'facebookdirect' THEN 'Facebook' WHEN url ~* 'googledirect' THEN 'Google' ELSE 'Other' END as channel, 
         campaign_id, ad_group_id, ad_id,
         COUNT(DISTINCT "order") as bookings_completed, COALESCE(SUM(subtotal),0) as subtotal_sales, COALESCE(SUM(total),0) as total_sales
     FROM initial_bookings_data
     WHERE state ~* 'final'
-    GROUP BY 1,2,3,4,5,6),
+    GROUP BY 1,2,3,4,5,6
+    {% if not loop.last %}UNION ALL
+    {% endif %}
+  {% endfor %}),
 
 initial_leads_data as 
     (SELECT *, {{ get_date_parts('created_on') }} FROM {{ source('gsheet_raw','crm_leads') }}),
 
 leads_data as
-    ({%- for date_granularity in date_granularity_list %}    
+  ({%- for date_granularity in date_granularity_list %}    
     SELECT '{{date_granularity}}' as date_granularity, {{date_granularity}} as date, 
         CASE WHEN source ~* 'facebook' THEN 'Facebook' WHEN source ~* 'google' THEN 'Google' ELSE 'Other' END as channel, 
         SPLIT_PART(utm_medium,' - ',1) as location, utm_campaign as campaign_name, utm_medium as ad_group_name,
         COUNT(opportunity_name) as leads
     FROM initial_leads_data
-    GROUP BY 1,2,3,4,5,6),
+    GROUP BY 1,2,3,4,5,6
+    {% if not loop.last %}UNION ALL
+    {% endif %}
+  {% endfor %}),
     
 fb_data as 
     (SELECT 'Facebook' as channel, campaign_name, campaign_type, ad_group_name, location, date, date_granularity, 
