954a2bdb5416b90550eab32551c98b7ae24d1277
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-11-14 16:44:28 -0600

    Update blended.sql
    service

--- models/reporting/blended.sql
+++ models/reporting/blended.sql
@@ -60,44 +60,95 @@ leads_data as
         CASE WHEN source ~* 'facebook' THEN 'Facebook' WHEN source ~* 'google' THEN 'Google' ELSE 'Other' END as channel, 
         CASE WHEN utm_medium ~* 'SB' THEN SPLIT_PART(utm_medium,' - ',2) WHEN utm_medium !~* 'SB' THEN SPLIT_PART(utm_medium,' - ',1) END as location,
         utm_campaign as campaign_name, utm_medium as ad_group_name,
+        CASE WHEN utm_content ~* 'lip flip' THEN 'Lip Flip'
+            WHEN utm_content ~* 'BBL' THEN 'BBL'
+            WHEN utm_content ~* 'hydrafacial' OR ad_name ~* 'diamondglow' THEN 'Facials'
+            WHEN utm_content ~* 'LHR' THEN 'LHR'
+            WHEN utm_content ~* 'Botox' THEN 'Tox'
+            WHEN utm_content ~* 'Filler' THEN 'Fillers'
+            WHEN utm_content ~* 'Morpheus' THEN 'Morpheus'
+            WHEN utm_content ~* 'coolsculpting' OR ad_name ~* 'emsculpt' THEN 'Body Contouring'
+            ELSE 'Others'
+        END AS service,
         COUNT(opportunity_name) as leads
     FROM {{ source('gsheet_raw','crm_leads') }}
-    GROUP BY 1,2,3,4,5,6
+    GROUP BY 1,2,3,4,5,6,7
     UNION ALL
     SELECT 'week' as date_granularity, DATE_TRUNC('week',created_on::date) as date,
         CASE WHEN source ~* 'facebook' THEN 'Facebook' WHEN source ~* 'google' THEN 'Google' ELSE 'Other' END as channel, 
         CASE WHEN utm_medium ~* 'SB' THEN SPLIT_PART(utm_medium,' - ',2) WHEN utm_medium !~* 'SB' THEN SPLIT_PART(utm_medium,' - ',1) END as location,
         utm_campaign as campaign_name, utm_medium as ad_group_name,
+        CASE WHEN utm_content ~* 'lip flip' THEN 'Lip Flip'
+            WHEN utm_content ~* 'BBL' THEN 'BBL'
+            WHEN utm_content ~* 'hydrafacial' OR ad_name ~* 'diamondglow' THEN 'Facials'
+            WHEN utm_content ~* 'LHR' THEN 'LHR'
+            WHEN utm_content ~* 'Botox' THEN 'Tox'
+            WHEN utm_content ~* 'Filler' THEN 'Fillers'
+            WHEN utm_content ~* 'Morpheus' THEN 'Morpheus'
+            WHEN utm_content ~* 'coolsculpting' OR ad_name ~* 'emsculpt' THEN 'Body Contouring'
+            ELSE 'Others'
+        END AS service,
         COUNT(opportunity_name) as leads
     FROM {{ source('gsheet_raw','crm_leads') }}
-    GROUP BY 1,2,3,4,5,6
+    GROUP BY 1,2,3,4,5,6,7
     UNION ALL
     SELECT 'month' as date_granularity, DATE_TRUNC('month',created_on::date) as date,
         CASE WHEN source ~* 'facebook' THEN 'Facebook' WHEN source ~* 'google' THEN 'Google' ELSE 'Other' END as channel, 
         CASE WHEN utm_medium ~* 'SB' THEN SPLIT_PART(utm_medium,' - ',2) WHEN utm_medium !~* 'SB' THEN SPLIT_PART(utm_medium,' - ',1) END as location,
         utm_campaign as campaign_name, utm_medium as ad_group_name,
+        CASE WHEN utm_content ~* 'lip flip' THEN 'Lip Flip'
+            WHEN utm_content ~* 'BBL' THEN 'BBL'
+            WHEN utm_content ~* 'hydrafacial' OR ad_name ~* 'diamondglow' THEN 'Facials'
+            WHEN utm_content ~* 'LHR' THEN 'LHR'
+            WHEN utm_content ~* 'Botox' THEN 'Tox'
+            WHEN utm_content ~* 'Filler' THEN 'Fillers'
+            WHEN utm_content ~* 'Morpheus' THEN 'Morpheus'
+            WHEN utm_content ~* 'coolsculpting' OR ad_name ~* 'emsculpt' THEN 'Body Contouring'
+            ELSE 'Others'
+        END AS service,
         COUNT(opportunity_name) as leads
     FROM {{ source('gsheet_raw','crm_leads') }}
-    GROUP BY 1,2,3,4,5,6
+    GROUP BY 1,2,3,4,5,6,7
     UNION ALL
     SELECT 'quarter' as date_granularity, DATE_TRUNC('quarter',created_on::date) as date,
         CASE WHEN source ~* 'facebook' THEN 'Facebook' WHEN source ~* 'google' THEN 'Google' ELSE 'Other' END as channel, 
         CASE WHEN utm_medium ~* 'SB' THEN SPLIT_PART(utm_medium,' - ',2) WHEN utm_medium !~* 'SB' THEN SPLIT_PART(utm_medium,' - ',1) END as location,
         utm_campaign as campaign_name, utm_medium as ad_group_name,
+        CASE WHEN utm_content ~* 'lip flip' THEN 'Lip Flip'
+            WHEN utm_content ~* 'BBL' THEN 'BBL'
+            WHEN utm_content ~* 'hydrafacial' OR ad_name ~* 'diamondglow' THEN 'Facials'
+            WHEN utm_content ~* 'LHR' THEN 'LHR'
+            WHEN utm_content ~* 'Botox' THEN 'Tox'
+            WHEN utm_content ~* 'Filler' THEN 'Fillers'
+            WHEN utm_content ~* 'Morpheus' THEN 'Morpheus'
+            WHEN utm_content ~* 'coolsculpting' OR ad_name ~* 'emsculpt' THEN 'Body Contouring'
+            ELSE 'Others'
+        END AS service,
         COUNT(opportunity_name) as leads
     FROM {{ source('gsheet_raw','crm_leads') }}
-    GROUP BY 1,2,3,4,5,6
+    GROUP BY 1,2,3,4,5,6,7
     UNION ALL
     SELECT 'year' as date_granularity, DATE_TRUNC('year',created_on::date) as date,
         CASE WHEN source ~* 'facebook' THEN 'Facebook' WHEN source ~* 'google' THEN 'Google' ELSE 'Other' END as channel, 
         CASE WHEN utm_medium ~* 'SB' THEN SPLIT_PART(utm_medium,' - ',2) WHEN utm_medium !~* 'SB' THEN SPLIT_PART(utm_medium,' - ',1) END as location,
         utm_campaign as campaign_name, utm_medium as ad_group_name,
+        CASE WHEN utm_content ~* 'lip flip' THEN 'Lip Flip'
+            WHEN utm_content ~* 'hydrafacial' THEN 'Hydrafacial'
+            WHEN utm_content ~* 'diamondglow' THEN 'Diamondglow'
+            WHEN utm_content ~* 'LHR' THEN 'LHR'
+            WHEN utm_content ~* 'Botox' THEN 'Botox'
+            WHEN utm_content ~* 'Facial Filler' THEN 'Facial Filler'
+            WHEN utm_content ~* 'Lip Filler' THEN 'Lip Filler'
+            WHEN utm_content ~* 'Microneedling' THEN 'Microneedling'
+            WHEN utm_content ~* 'coolsculpting' OR utm_content ~* 'emsculpt' OR utm_content ~* 'body contouring' THEN 'Body Contouring'
+            ELSE 'Others'
+        END AS service,
         COUNT(opportunity_name) as leads
     FROM {{ source('gsheet_raw','crm_leads') }}
-    GROUP BY 1,2,3,4,5,6),
+    GROUP BY 1,2,3,4,5,6,7),
     
 fb_data as 
-    (SELECT 'Facebook' as channel, campaign_name, campaign_type, ad_group_name, location, date, date_granularity, 
+    (SELECT 'Facebook' as channel, campaign_name, campaign_type, ad_group_name, location, service, date, date_granularity, 
         COALESCE(SUM(spend),0) as spend, COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(clicks),0) as clicks, 
         COALESCE(SUM(bookings_completed),0) as bookings_completed, COALESCE(SUM(leads),0) as leads, COALESCE(SUM(appointments_scheduled),0) as appointments_scheduled,
         COALESCE(SUM(platform_leads),0) as platform_leads 
@@ -109,7 +160,7 @@ fb_data as
                 WHEN campaign_name = '[SB] - DTB - Catch All - Black Friday 2024' THEN 'Black Friday DTB'
                 ELSE 'Other'
             END as campaign_type,
-            adset_name as ad_group_name, location, date, date_granularity, 
+            adset_name as ad_group_name, location, service, date, date_granularity, 
             spend, impressions, link_clicks as clicks, 0 as bookings_completed, 0 as leads, appointments_scheduled, leads as platform_leads
         FROM {{ source('reporting','facebook_ad_performance') }}
         UNION ALL
@@ -121,12 +172,12 @@ fb_data as
                 ELSE 'Other'
             END as campaign_type,
             ad_group_name, CASE WHEN ad_group_name ~* 'SB' THEN SPLIT_PART(ad_group_name,' - ',2) WHEN ad_group_name !~* 'SB' THEN SPLIT_PART(ad_group_name,' - ',1) END as location, 
-            date, date_granularity,
+            service, date, date_granularity,
             0 as spend, 0 as impressions, 0 as clicks, COALESCE(SUM(bookings_completed),0) as bookings_completed, 0 as leads, 0 as appointments_scheduled, 0 as platform_leads
         FROM bookings_data
         LEFT JOIN (SELECT DISTINCT campaign_id::text, campaign_name, adset_id::text as ad_group_id, adset_name as ad_group_name, ad_id::text FROM {{ source('reporting','facebook_ad_performance') }}) USING(campaign_id,ad_id)   
         WHERE channel = 'Facebook'
-        GROUP BY 1,2,3,4,5,6
+        GROUP BY 1,2,3,4,5,6,7
         UNION ALL
         SELECT campaign_name, 
             CASE WHEN campaign_name = '[SB] - Prospecting - DTB' THEN 'Prospecting DTB' 
@@ -135,46 +186,46 @@ fb_data as
                 WHEN campaign_name = '[SB] - DTB - Catch All - Black Friday 2024' THEN 'Black Friday DTB'
                 ELSE 'Other'
             END as campaign_type,
-            ad_group_name, location, date, date_granularity,
+            ad_group_name, location, service, date, date_granularity,
             0 as spend, 0 as impressions, 0 as clicks, 0 as bookings_completed, COALESCE(SUM(leads),0) as leads, 0 as appointments_scheduled, 0 as platform_leads
         FROM leads_data
         WHERE channel = 'Facebook'
         GROUP BY 1,2,3,4,5,6          
         )
-    GROUP BY 1,2,3,4,5,6,7),
+    GROUP BY 1,2,3,4,5,6,7,8),
 
 adw_data as 
-    (SELECT 'Google' as channel, campaign_name, campaign_type, ad_group_name, location, date, date_granularity, 
+    (SELECT 'Google' as channel, campaign_name, campaign_type, ad_group_name, location, service, date, date_granularity, 
         COALESCE(SUM(spend),0) as spend, COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(clicks),0) as clicks, 
         COALESCE(SUM(bookings_completed),0) as bookings_completed, COALESCE(SUM(leads),0) as leads, COALESCE(SUM(appointments_scheduled),0) as appointments_scheduled,
         COALESCE(SUM(platform_leads),0) as platform_leads
     FROM
         (SELECT campaign_name, 
             CASE WHEN campaign_name ~* 'Search - Branded' THEN 'Search - Branded' WHEN campaign_name ~* 'Search - Non Brand' THEN 'Search - Non Brand' WHEN campaign_name ~* 'PMax' THEN 'PMax' END as campaign_type,
-            ad_group_name, null as location, date, date_granularity, 
+            ad_group_name, null as location, null as service, date, date_granularity, 
             spend, impressions, clicks, 0 as bookings_completed, 0 as leads, appointments_scheduled, 0 as platform_leads
         FROM {{ source('reporting','googleads_ad_performance') }}
         UNION ALL
         SELECT campaign_name, 
             CASE WHEN campaign_name ~* 'Search - Branded' THEN 'Search - Branded' WHEN campaign_name ~* 'Search - Non Brand' THEN 'Search - Non Brand' WHEN campaign_name ~* 'PMax' THEN 'PMax' END as campaign_type,
-            ad_group_name, null as location, date, date_granularity,
+            ad_group_name, null as location, null as service, date, date_granularity,
             0 as spend, 0 as impressions, 0 as clicks, COALESCE(SUM(bookings_completed),0) as bookings_completed, 0 as leads, 0 as appointments_scheduled, 0 as platform_leads
         FROM bookings_data
         LEFT JOIN (SELECT DISTINCT campaign_id::text, campaign_name, ad_group_id::text, ad_group_name FROM {{ source('reporting','googleads_ad_performance') }}) USING(campaign_id,ad_group_id)   
         WHERE channel = 'Google'
-        GROUP BY 1,2,3,4,5,6
+        GROUP BY 1,2,3,4,5,6,7
         UNION ALL
         SELECT campaign_name, 
             CASE WHEN campaign_name ~* 'Search - Branded' THEN 'Search - Branded' WHEN campaign_name ~* 'Search - Non Brand' THEN 'Search - Non Brand' WHEN campaign_name ~* 'PMax' THEN 'PMax' END as campaign_type,
-            ad_group_name, null as location, date, date_granularity,
+            ad_group_name, null as location, null as service, date, date_granularity,
             0 as spend, 0 as impressions, 0 as clicks, 0 as bookings_completed, COALESCE(SUM(leads),0) as leads, 0 as appointments_scheduled, 0 as platform_leads
         FROM leads_data
         WHERE channel = 'Google'
-        GROUP BY 1,2,3,4,5,6
+        GROUP BY 1,2,3,4,5,6,7
         )
-    GROUP BY 1,2,3,4,5,6,7)
+    GROUP BY 1,2,3,4,5,6,7,8)
 
-SELECT channel, campaign_name, campaign_type, ad_group_name, location, date, date_granularity, spend, impressions, clicks, bookings_completed, leads, appointments_scheduled, platform_leads
+SELECT channel, campaign_name, campaign_type, ad_group_name, location, service, date, date_granularity, spend, impressions, clicks, bookings_completed, leads, appointments_scheduled, platform_leads
 FROM 
     (SELECT * FROM fb_data
     UNION ALL 
